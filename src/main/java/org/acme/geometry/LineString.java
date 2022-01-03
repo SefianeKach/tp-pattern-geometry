828a611f46d0a33e30bd10b6a5ab9c87093e5b9f
KSefiane <sefiane.kachouani@ensg.eu>
2022-01-03 16:20:31 +0100

    Finalizing Geometry, Point, LineString Classes and Tests

--- src/main/java/org/acme/geometry/LineString.java
+++ src/main/java/org/acme/geometry/LineString.java
@@ -1,30 +1,34 @@
 package org.acme.geometry;
 
 import java.util.List;
+import java.util.ArrayList;
 
-public class LineString implements Geometry{
-    
-    private List<Point> points;
 
-    public LineString() {
-        this.points = null;    
+public class LineString implements Geometry {
+
+private List<Point> points;
+
+public LineString() {
+        this.points = new ArrayList<Point>();
+        this.points.add(new Point());
     }
 
-    public LineString(List<Point> points){
+
+    public LineString(List<Point> points) {
         this.points = points;
     }
 
-    public int getNumPoints(){
-        return points.size();  
+    public int getNumPoints() {
+        return points.size();
     }
-         
-    public Point getPointN(int n){
-        return points.get(n); 
+
+    public Point getPointN(int n) {
+        return points.get(n);
     }
+
 
     @Override
     public String getType() {
         return "LineString";
     }
-
 }
--- src/test/java/org/acme/geometry/LineStringTest.java
+++ src/test/java/org/acme/geometry/LineStringTest.java
@@ -1,5 +1,76 @@
 package org.acme.geometry;
 
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class LineStringTest {
+
+    public static final double EPSILON = 1.0e-15;
+
+	public static Point createPointO() {
+        return new Point(new Coordinate());
+    }
+        
+    public static Point createPointA() {
+        return new Point(new Coordinate(0.8, 1.2));
+    }   
+        
+    @Test
+    public void testDefaultConstructor() {
+        LineString l = new LineString();
+        Assert.assertEquals(1, l.getNumPoints());
+    }
+    
+ 
+    @Test
+    public void testConstructor() {
+        List<Point> points = new ArrayList<Point>();
+        points.add(createPointA());
+        LineString l = new LineString(points);
+        Assert.assertEquals(1, l.getNumPoints());
+    }
+    
+    
+    
+    @Test
+    public void getTypeMethod() {     
+        LineString l = new LineString();
+        Assert.assertEquals("LineString", l.getType());
+    }
+    
+    
+    
+    @Test
+    public void getNumPointsMethod() {
+        List<Point> points = new ArrayList<Point>();
+        points.add(createPointO());
+        points.add(createPointA());
+        LineString l = new LineString(points);
+        Assert.assertEquals(2, l.getNumPoints(), EPSILON);
+    }
+    
+    
+    
+    @Test
+    public void getPointNMethod() {
+        List<Point> points = new ArrayList<Point>();
+        points.add(createPointO());
+        points.add(createPointA());
+        LineString l = new LineString(points);
+        Assert.assertEquals("Point", l.getPointN(0).getType());
+    }
+    
+    
     
 }
+
+
+
+
+
+
+
