688ffcf49a997822a54bf9f50f8424ed514adfac
KSefiane <sefiane.kachouani@ensg.eu>
2022-01-03 14:03:26 +0100

    creating Coordinate class

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,36 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    
+    double x ;
+    double y ;
+
+    public Coordinate() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+    
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
 }
+
+public void Coordinate() {
+    
+}
+
+public void Coordinate(double x, double y) {
+    
+}
+
+
+public double getX(){
+    return x;
+}
+
+public double getY(){
+    return y;
+}
+
