303fad724969fa6264c00497fd3287a36ef37923
KSefiane <sefiane.kachouani@ensg.eu>
2022-01-03 15:05:14 +0100

    edditing LineString.java

--- src/main/java/org/acme/geometry/LienString.java
+++ src/main/java/org/acme/geometry/LienString.java
@@ -1,5 +1,30 @@
 package org.acme.geometry;
 
+import java.util.List;
+
 public class LienString implements Geometry{
     
+    private List<Point> points;
+
+    public void LineString() {
+        this.points = null;    
+    }
+
+    public void LineString(List<Point> points){
+        this.points = points;
+    }
+
+    public int getNumPoints(){
+        return points.size();  
+    }
+         
+    public Point getPointN(int n){
+        return points.get(n); 
+    }
+
+    @Override
+    public String getType() {
+        return "LineString";
+    }
+
 }
