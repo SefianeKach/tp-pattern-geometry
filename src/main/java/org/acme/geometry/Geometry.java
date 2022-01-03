791ea8de98a83075e54882a72ed4fc347a4459db
KSefiane <sefiane.kachouani@ensg.eu>
2022-01-03 16:57:06 +0100

    adding translate method

--- src/main/java/org/acme/geometry/Geometry.java
+++ src/main/java/org/acme/geometry/Geometry.java
@@ -1,7 +1,8 @@
 package org.acme.geometry;
 
-interface Geometry {
-   
+interface Geometry { 
     public String getType(); 
     public boolean isEmpty();
+
+    public void translate (double dx, double dy);
 }
